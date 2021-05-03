97258acc2644c74ada958f33730ee0b1111ca779
ZhaoqingLiu <Zhaoqing.Liu@uts.edu.au>
2021-05-03 14:37:43 +1000

    Update Multi-Task mode

--- exp_params.py
+++ exp_params.py
@@ -25,29 +25,27 @@ class ComparisionMode(Enum):
 # 30 for each Laureate cluster, 26 for each Mercury cluster, and 8 for each Venus cluster.
 NUM_CPU_CORES = multiprocessing.cpu_count()
 
-
 # The data sets on which you want to run experiments.
 DS_LOAD_FUNC_CLF = {"Diabetes": load_diabetes}
 # DS_LOAD_FUNC_CLF = {"Vehicle": load_vehicle, "German_Credit": load_German_credit, "Diabetes": load_diabetes, "Iris": load_iris, "Wine": load_wine}
 DS_LOAD_FUNC_REG = {}
 
-
 # Searching an optimum fuzzy threshold by a loop according the specified stride.
-FUZZY_TH = 0.0
 FUZZY_STRIDE = 0.01
 
-
-print("==================================================================")
+for _ in range(80): print("=", end="")
+print("")
 print("                    Configuration Information                   ")
 
 print("Comparison experiments include:")
 for k, name in ComparisionMode.__members__.items():
-    print("                               %s -- %s" % (k, name.value))
+    print("{:>80}".format(k + " -- " + name.value))
 
-print("Number of CPU cores available on the current server:")
-print("                               %s" % NUM_CPU_CORES)
+print("Number of CPU cores available:")
+print("{:>80}".format(NUM_CPU_CORES))
 
 print("Data sets to train on:")
 for ds_name in DS_LOAD_FUNC_CLF.keys():
-    print("                               %s" % (ds_name))
-print("==================================================================")
+    print("{:>80}".format((ds_name)))
+for _ in range(80): print("=", end="")
+print("")
--- exp_s2.py
+++ exp_s2.py
@@ -9,13 +9,10 @@ import os
 import time
 
 import numpy as np
-import pandas as pd
-from sklearn import datasets
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import KFold
-from sklearn.tree import DecisionTreeClassifier
 
-from exp_params import ComparisionMode, NUM_CPU_CORES, DS_LOAD_FUNC_CLF, FUZZY_TH, FUZZY_STRIDE
+from exp_params import ComparisionMode, NUM_CPU_CORES, DS_LOAD_FUNC_CLF, FUZZY_STRIDE
 from fuzzy_trees.fuzzy_decision_tree import FuzzyDecisionTreeClassifier
 from fuzzy_trees.fuzzy_decision_tree_api import FuzzificationParams, FuzzyDecisionTreeClassifierAPI, CRITERIA_FUNC_CLF, \
     CRITERIA_FUNC_REG
@@ -24,19 +21,102 @@ from fuzzy_trees.util_data_processing_funcs import extract_fuzzy_features
 import fuzzy_trees.util_plotter as plotter
 
 
-def search_optimum_fuzzy_th(conn_send, comparing_mode, dataset_name, fuzzy_th):
+def search_fuzzy_optimum():
+    """
+    Task 1: Searching an optimum of fuzzy thresholds by a loop according the specified stride.
+    TODO:
+        1. Move the multi-process mode here to all algorithms in fuzzy_decision_tree_api.py
+        2. Add pre-train to the algorithm to help developers set the super-parameter "fuzzy_threshold" or add post-
+    """
+    # Create a connection between processes.
+    # NB: When using Pool create processes, use multiprocessing.Manager().Queue()
+    # instead of multiprocessing.Queue() to create connection.
+    conn_recv, conn_send = multiprocessing.Pipe()
+
+    # Create a pool containing n (0 - infinity) processes.
+    # If the parameter "processes" is None then the number returned by os.cpu_count() is used.
+    # Make sure that n is <= the number of CPU cores available.
+    # The parameters to the Pool indicate how many parallel processes are called to run the program.
+    # The default size of the Pool is the number of compute cores on the CPU, i.e. multiprocessing.cpu_count().
+    pool = multiprocessing.Pool(NUM_CPU_CORES)
+
+    # Complete all tasks by the pool.
+    # !!! NB: If you want to complete the experiment faster, you can use distributed computing. Or you can divide
+    # the task into k groups to execute in k py programs, and then run one on each of k clusters simultaneously.
+    fuzzy_th = 0
+    for ds_name in DS_LOAD_FUNC_CLF.keys():
+        while fuzzy_th <= 0.5:
+            # Add a process into the pool. apply_async() is asynchronous equivalent of "apply()" builtin.
+            res = pool.apply_async(search_fuzzy_optimum_on_one_ds,
+                                   args=(conn_send, ComparisionMode.FUZZY, ds_name, fuzzy_th,))
+            fuzzy_th += FUZZY_STRIDE
+
+    pool.close()
+    pool.join()
+
+    # Read the result and prepare the data for plotting.
+    # Read result from the queue "q" and save them.
+    x_list_train = []
+    y_list_train = []
+    x_list_test = []
+    y_list_test = []
+    while conn_recv.poll():
+        res_list = conn_recv.recv()
+        dataset_name = res_list[0][0]  # TODO: prepare a dataset_name_list, and then uniques()
+        x_list_train.append(res_list[0][0])
+        y_list_train.append(res_list[0][1])
+        x_list_test.append(res_list[1][0])
+        y_list_test.append(res_list[1][1])
+        print(x_list_train)
+        print(y_list_train)
+        print(x_list_test)
+        print(y_list_test)
+
+    # Sort fuzzy_th_list in ascending order, and then sort accuracy_list in the same order as fuzzy_th_list.
+    # Because multiple processes may not return results in an ascending order of fuzzy thresholds.
+    print("before sorting:")
+    print(x_list_train)
+    x_train = sorted(x_list_train)
+    y_train = [y for _, y in sorted(zip(x_list_train, y_list_train))]  # Default to the ascending order of 1st list in zip().
+    print("after sorting:")
+    print(y_train)
+    x_test = sorted(x_list_test)
+    y_test = [y for _, y in sorted(zip(x_list_test, y_list_test))]
+
+    # Plot the comparison of training error versus test error, and both curves are fuzzy thresholds versus accuracies.
+    # Illustrate how the performance on unseen data (test data) is different from the performance on training data.
+    assert (len(x_train) > 0 and len(y_train) > 0)
+    x_lower_limit, x_upper_limit = np.min(x_train), np.max(x_train)
+    y_lower_limit = np.min(y_train) if np.min(y_train) < np.min(y_test) else np.min(y_test)
+    y_upper_limit = np.max(y_train) if np.max(y_train) > np.max(y_test) else np.max(y_test)
+    print("x_limits and y_limits are:", x_lower_limit, x_upper_limit, y_lower_limit, y_upper_limit)
+    plotter.plot_multi_curves(x=[x_train, x_test],
+                              y=[y_train, y_test],
+                              title="Training Error vs Test Error on {%s}".format(dataset_name),
+                              x_label="Fuzzy threshold",
+                              y_label="Performance",
+                              x_limit=(x_lower_limit, x_upper_limit),
+                              y_limit=(y_lower_limit - (y_lower_limit / 100), y_upper_limit + (y_upper_limit / 100)),
+                              legends=["Train", "Test"])
+
+
+def search_fuzzy_optimum_on_one_ds(conn_send, comparing_mode, dataset_name, fuzzy_th):
     # Load all data sets.
     ds_df = load_dataset_clf(dataset_name)
 
-    # Execute the experiment according to the parameters.
+    # Run the experiment according to the parameters.
     X = ds_df.iloc[:, :-1].values
     y = ds_df.iloc[:, -1].values
     accuracy_train_list, accuracy_test_list = get_exp_results_clf(X, y, comparing_mode=comparing_mode, dataset_name=dataset_name, fuzzy_th=fuzzy_th)
-    accuracy_train_mean = np.mean(accuracy_train_list)
-    accuracy_test_mean = np.mean(accuracy_test_list)
 
-    # Put the result in "queue".
-    conn_send.send([[fuzzy_th, accuracy_train_mean], [fuzzy_th, accuracy_test_mean]])
+    # Process the result.
+    accuracy_train_mean = np.mean(accuracy_train_list)
+    error_train_mean = 1 - accuracy_train_mean
+    accuracy_test_mean = np.mean(accuracy_test_list)
+    error_test_mean = 1 - accuracy_test_mean
+
+    # Put the result in the connection between the main process and the child processes (in master-worker mode).
+    conn_send.send([[fuzzy_th, error_train_mean], [fuzzy_th, error_test_mean]])
 
 
 def load_dataset_clf(dataset_name):
@@ -63,7 +143,7 @@ def get_exp_results_clf(X, y, comparing_mode, dataset_name, fuzzy_th):
     accuracy_train_list = []
     accuracy_test_list = []
 
-    # Preprocess features for using fuzzy decision tree.
+    # Step 1: Preprocess features for using fuzzy decision tree.
     X_fuzzy_pre = X.copy()
     fuzzification_params = None
     X_dms = None
@@ -86,6 +166,7 @@ def get_exp_results_clf(X, y, comparing_mode, dataset_name, fuzzy_th):
     X_plus_dms = np.concatenate((X, X_dms), axis=1)
     print("************* X_plus_dms's shape:", np.shape(X_plus_dms))
 
+    # Step 2: Get training and testing result by a model.
     for i in range(10):
         print("%ith comparison on %s" % (i, dataset_name))
 
@@ -98,8 +179,8 @@ def get_exp_results_clf(X, y, comparing_mode, dataset_name, fuzzy_th):
 
             # Using a fuzzy decision tree. =============================================================================
             X_train, X_test = X_plus_dms[train_index], X_plus_dms[test_index]
-            accuracy_train, accuracy_test = use_fuzzy_trees(comparing_mode=comparing_mode, X_train=X_train, X_test=X_test, y_train=y_train,
-                                       y_test=y_test, fuzzification_params=fuzzification_params)
+            accuracy_train, accuracy_test = exe_by_a_fuzzy_model(comparing_mode=comparing_mode, X_train=X_train, X_test=X_test, y_train=y_train,
+                                                                 y_test=y_test, fuzzification_params=fuzzification_params)
             accuracy_train_list.append(accuracy_train)
             accuracy_test_list.append(accuracy_test)
 
@@ -114,9 +195,10 @@ def get_exp_results_clf(X, y, comparing_mode, dataset_name, fuzzy_th):
     return accuracy_train_list, accuracy_test_list
 
 
-def use_fuzzy_trees(comparing_mode, X_train, X_test, y_train, y_test, fuzzification_params):
+def exe_by_a_fuzzy_model(comparing_mode, X_train, X_test, y_train, y_test, fuzzification_params):
     time_start = time.time()  # Record the start time.
 
+    # Initialise a fuzzy model.
     clf = None
     if comparing_mode is ComparisionMode.NAIVE:
         # My NDT vs. sklearn NDT
@@ -143,13 +225,18 @@ def use_fuzzy_trees(comparing_mode, X_train, X_test, y_train, y_test, fuzzificat
         clf = FuzzyGBDTClassifier(disable_fuzzy=False, fuzzification_params=fuzzification_params,
                                   is_trees_mixed=True, criterion_func=CRITERIA_FUNC_REG["mse"], learning_rate=0.1,
                                   n_estimators=100,
-                                  max_depth=5)  # TODO: Add one more argument "is_trees_mixed", default=False.
+                                  max_depth=5)
+
+    # Fit the initialised model.
     clf.fit(X_train, y_train)
     # clf.print_tree()
+
+    # Get the training accuracy and test accuracy of the fitted (trained) estimator.
     y_pred_train = clf.predict(X_train)
     accuracy_train = accuracy_score(y_train, y_pred_train)
     y_pred_test = clf.predict(X_test)
     accuracy_test = accuracy_score(y_test, y_pred_test)
+
     print("    Fuzzy accuracy train:", accuracy_train)
     print("    Fuzzy accuracy test:", accuracy_test)
     print('    Elapsed time (FDT-based):', time.time() - time_start, 's')  # Display the time of training a model.
@@ -159,88 +246,15 @@ def use_fuzzy_trees(comparing_mode, X_train, X_test, y_train, y_test, fuzzificat
 
 """
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-In master-worker mode, if an execution in the master process depends on the
-results of all the child processes, place its code after either Pool().join() 
-or Process().join(). 
-Never put it in a child process because the child Processes are parallel to 
-each other, so you don't know if the other child processes are terminated if
-you are in one of the child processes.
+In master-worker mode, if an execution in the master process depends on the results of all the child processes, place this execution code after either Pool().join() or Process().join(). 
+The reason it is not recommended to put execution code in child processes is because child processes run in parallel with each other, so if it is in a child process, it doesn't know if the other child processes have terminated. Although it is possible to obtain the status of all child processes through a two-way Pipe(), that is not a good management mode.
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 """
 if __name__ == '__main__':
-    time_start = time.time()
     print("Main Process (%s) started." % os.getpid())
+    time_start = time.time()
 
-    # Create a connection between processes.
-    # NB: When using Pool create processes, use multiprocessing.Manager().Queue()
-    # instead of multiprocessing.Queue() to create connection.
-    conn_recv, conn_send = multiprocessing.Pipe()
-
-    # Create a pool containing n (0 - infinity) processes.
-    # If the parameter "processes" is None then the number returned by os.cpu_count() is used.
-    # Make sure that n is <= the number of CPU cores available.
-    # The parameters to the Pool indicate how many parallel processes are called to run the program.
-    # The default size of the Pool is the number of compute cores on the CPU, i.e. multiprocessing.cpu_count().
-    pool = multiprocessing.Pool(NUM_CPU_CORES)
-
-    # Complete all tasks by the pool.
-    # !!! NB: If you want to complete the experiment faster, you can use distributed computing. Or you can divide
-    # the task into k groups to execute in k py programs, and then run one on each of k clusters simultaneously.
-    for ds_name in DS_LOAD_FUNC_CLF.keys():
-        # TODO: 1st task: Searching an optimum fuzzy threshold by a loop according the specified stride.
-        while FUZZY_TH <= 0.5:
-            # Add a process into the pool. apply_async() is asynchronous equivalent of "apply()" builtin.
-            err = pool.apply_async(search_optimum_fuzzy_th, args=(conn_send, ComparisionMode.FUZZY, ds_name, FUZZY_TH,))
-            FUZZY_TH += FUZZY_STRIDE
-
-    pool.close()
-    pool.join()
-
-    # Read the result and prepare the data for plotting.
-    # Read result from the queue "q" and save them.
-    x_list_train = []
-    y_list_train = []
-    x_list_test = []
-    y_list_test = []
-    while conn_recv.poll():
-        res_list = conn_recv.recv()
-        x_list_train.append(res_list[0][0])
-        y_list_train.append(res_list[0][1])
-        x_list_test.append(res_list[1][0])
-        y_list_test.append(res_list[1][1])
-        print(x_list_train)
-        print(y_list_train)
-        print(x_list_test)
-        print(y_list_test)
-        print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-        print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-
-    # TODO: If needed, sort fuzzy_th_list in ascending order, and then sort accuracy_list in the same order as fuzzy_th_list.
-    # Because multiple processes may not return results in an ascending order of fuzzy thresholds.
-    print("before sorting:")
-    print(x_list_train)
-    x_train = sorted(x_list_train)
-    y_train = [y for _, y in sorted(zip(x_list_train, y_list_train))]
-    print("after sorting:")
-    print(y_train)
-    x_test = sorted(x_list_test)
-    y_test = [y for _, y in sorted(zip(x_list_test, y_list_test))]
-
-    # TODO: Plot the comparison of training error versus test error, and both curves are fuzzy thresholds versus accuracies.
-    # Illustrate how the performance on unseen data (test data) is different from the performance on training data.
-    assert (len(x_train) > 0 and len(y_train) > 0)
-    x_lower_limit, x_upper_limit = np.min(x_train), np.max(x_train)
-    y_lower_limit = np.min(y_train) if np.min(y_train) < np.min(y_test) else np.min(y_test)
-    y_upper_limit = np.max(y_train) if np.max(y_train) > np.max(y_test) else np.max(y_test)
-    print("x_limits and y_limits are:", x_lower_limit, x_upper_limit, y_lower_limit, y_upper_limit)
-    plotter.plot_multi_curves(x=[x_train, x_test],
-                              y=[y_train, y_test],
-                              title="Training Error vs Test Error",
-                              x_label="Fuzzy threshold",
-                              y_label="Performance",
-                              x_limit=(x_lower_limit, x_upper_limit),
-                              y_limit=(y_lower_limit - (y_lower_limit / 100), y_upper_limit + (y_upper_limit / 100)),
-                              legends=["Train", "Test"])
+    search_fuzzy_optimum()
 
     print("Total elapsed time: {:.5}s".format(time.time() - time_start))
     print("Main Process (%s) ended." % os.getpid())
